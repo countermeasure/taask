d2a2fbcfb8a5d859f017d8934b9697257e5b24ca
Sky <sky@skychristensen.com>
2014-10-02 12:54:36 +1000

    Replaced underscores with hyphens in urls

--- taskwarrior_django/urls.py
+++ taskwarrior_django/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^add_task$', 'tasks.views.add_task', name='add-task'),
-    url(r'^list_tasks$', 'tasks.views.list_tasks',
+    url(r'^add-task$', 'tasks.views.add_task', name='add-task'),
+    url(r'^list-tasks$', 'tasks.views.list_tasks',
         name='list-tasks'),
-    url(r'^edit_task/(\d{1,6})/$', 'tasks.views.edit_task', name='edit-task'),
+    url(r'^edit-task/(\d{1,6})/$', 'tasks.views.edit_task', name='edit-task'),
 )
