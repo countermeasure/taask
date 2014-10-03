9dba29ca3d03c11270b70ebe9eedddc9bd79a616
Sky <sky@skychristensen.com>
2014-10-03 22:34:19 +1000

    Basic CRUD and task list working smoothly

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,16 +1,136 @@
 from django import forms
 
 
-class TaskForm(forms.Form):
+class AddTaskForm(forms.Form):
 
+    # 'description' is a string which describes the task
     description = forms.CharField(max_length=200)
+
+    # 'tags' is a list of strings, where each string is a single word containing
+    # no spaces. For example:
+    # ["home","garden"]
+    # We'll collect several tags then merge them to create the tags list
+
+    # Tag which defines the view
+    VIEW_CHOICES = (
+        ('inbox', 'Inbox'),
+        ('today', 'Today'),
+        ('next', 'Next'),
+        ('someday', 'Someday'),
+        )
+    tag_view = forms.ChoiceField(choices=VIEW_CHOICES,
+                                 label='View')
+
+    # 'priority' is a string which contains 'H', 'M' or 'L'
+    # Either 'priority' or 'tag_importance' will be used longer term. 'priority'
+    # should be preferred as it is the built-in option.
+    PRIORITY_CHOICES = (
+        (None, ''),
+        ('H', 'High'),
+        ('M', 'Medium'),
+        ('L', 'Low'),
+        )
+    priority = forms.ChoiceField(choices=PRIORITY_CHOICES, required=False)
+
+    # Tag which defines the estimated time for a task
+    TIME_CHOICES = (
+        (None, ''),
+        ('5', '5mins'),
+        ('15', '15mins'),
+        ('30', '30mins'),
+        ('60', '1hr'),
+        ('120', '2hrs'),
+        ('300', '5hrs'),
+        )
+    tag_time = forms.ChoiceField(choices=TIME_CHOICES, label='Time',
+                                 required=False)
+
+    # 'project' is a string which gives the name of the project.
     project = forms.CharField(max_length=200, required=False)
-    priority = forms.CharField(max_length=200, required=False)
-    due = forms.CharField(max_length=200, required=False)
-    recur = forms.CharField(max_length=200, required=False)
-    until = forms.CharField(max_length=200, required=False)
-    wait = forms.CharField(max_length=200, required=False)
-    scheduled = forms.CharField(max_length=200, required=False)
-    depends = forms.CharField(max_length=200, required=False)
-    annotations = forms.CharField(max_length=200, required=False)
-    tags = forms.CharField(max_length=200, required=False)
+
+    # 'due' is a date on which the task should be finished
+    due = forms.DateTimeField(required=False, label='Due date')
+
+    # 'recur' is a string which represents the interval between recurring tasks,
+    # such as '3wks'
+    recur = forms.CharField(max_length=200, required=False, label='Frequency')
+
+    # 'until' is a date on which a recurring task will cease to recur
+    until = forms.DateTimeField(required=False, label='Recurs until')
+
+    # 'wait' is a date on which a task with status 'waiting' will have it's
+    # status changed to 'pending'
+    wait = forms.DateTimeField(required=False, label='Schedule for')
+
+
+class EditTaskForm(forms.Form):
+
+    # 'description' is a string which describes the task
+    description = forms.CharField(max_length=200)
+
+    # 'tags' is a list of strings, where each string is a single word containing
+    # no spaces. For example:
+    # ["home","garden"]
+    # We'll collect several tags then merge them to create the tags list
+
+    # Tag which defines the view
+    VIEW_CHOICES = (
+        ('inbox', 'Inbox'),
+        ('today', 'Today'),
+        ('next', 'Next'),
+        ('someday', 'Someday'),
+        ('rubbish', 'Rubbish'),
+        )
+    tag_view = forms.ChoiceField(choices=VIEW_CHOICES,
+                                 label='View')
+
+    # 'priority' is a string which contains 'H', 'M' or 'L'
+    PRIORITY_CHOICES = (
+    #    (None, ''),
+        ('H', 'High'),
+        ('M', 'Medium'),
+        ('L', 'Low'),
+        )
+    priority = forms.ChoiceField(choices=PRIORITY_CHOICES)
+
+    # Tag which defines the order of tasks in the tables
+    ORDER_CHOICES = (
+        (None, ''),
+        ('first', '1st'),
+        ('second', '2nd'),
+        ('third', '3rd'),
+        ('fourth', '4th'),
+        ('fifth', '5th'),
+        )
+    tag_order = forms.ChoiceField(choices=ORDER_CHOICES,
+                                  label='Order', required=False)
+
+    # Tag which defines the estimated time for a task
+    TIME_CHOICES = (
+        (None, ''),
+        ('5', '5mins'),
+        ('15', '15mins'),
+        ('30', '30mins'),
+        ('60', '1hr'),
+        ('120', '2hrs'),
+        ('300', '5hrs'),
+        )
+    tag_time = forms.ChoiceField(choices=TIME_CHOICES, label='Time',
+                                 required=False)
+
+    # 'project' is a string which gives the name of the project.
+    project = forms.CharField(max_length=200, required=False)
+
+    # 'due' is a date on which the task should be finished
+    due = forms.DateTimeField(required=False, label='Due date')
+
+    # 'recur' is a string which represents the interval between recurring tasks,
+    # such as '3wks'
+    recur = forms.CharField(max_length=200, required=False, label='Frequency')
+
+    # 'until' is a date on which a recurring task will cease to recur
+    until = forms.DateTimeField(required=False, label='Recurs until')
+
+    # 'wait' is a date on which a task with status 'waiting' will have it's
+    # status changed to 'pending'
+    wait = forms.DateTimeField(required=False, label='Schedule for')
--- tasks/views.py
+++ tasks/views.py
@@ -2,38 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from tasks.forms import TaskForm
-
+from tasks.forms import AddTaskForm, EditTaskForm
 from taskw import TaskWarrior
 
 
 w = TaskWarrior(marshal=True)
-# w = TaskWarrior(config_filename="~/some_project/.taskrc")
+# w = TaskWarrior(config_filename="/path/to/.taskrc")
 
 
 def add_task(request):
     """Add a task."""
+
     if request.method == "POST":
-        form = TaskForm(request.POST, label_suffix='')
+        form = AddTaskForm(request.POST, label_suffix='')
         if form.is_valid():
+            # Assign each cleaned data item to its own variable
             description = form.cleaned_data['description']
-            project = form.cleaned_data['project']
+            tag_view = form.cleaned_data['tag_view']
             priority = form.cleaned_data['priority']
+            tag_time = form.cleaned_data['tag_time']
+            project = form.cleaned_data['project']
             due = form.cleaned_data['due']
             recur = form.cleaned_data['recur']
             until = form.cleaned_data['until']
             wait = form.cleaned_data['wait']
-            scheduled = form.cleaned_data['scheduled']
-            depends = form.cleaned_data['depends']
-            annotations = form.cleaned_data['annotations']
-            tags = form.cleaned_data['tags']
-            w.task_add(description, project=project, priority=priority,
-                       due=due, recur=recur, until=until, wait=wait,
-                       scheduled=scheduled, depends=depends,
-                       annotations=annotations, tags=tags)
+            # Construct 'tags' in the format which taskw expects
+            # tags[1] is an empty string as it's a placeholder for tag_order
+            tags = [tag_view, '', tag_time]
+            # Create the new task
+            w.task_add(description,
+                       priority=priority,
+                       project=project,
+                       due=due,
+                       recur=recur,
+                       until=until,
+                       wait=wait,
+                       tags=tags)
             return HttpResponseRedirect(reverse('list-tasks'))
     else:
-        form = TaskForm()
+        form = AddTaskForm()
 
     return render(request, 'add_task.html', {
                            'form': form,
@@ -42,6 +49,7 @@ def add_task(request):
 
 def list_tasks(request):
     """Lists all pending tasks."""
+
     tasks = w.load_tasks()
     task_list = tasks['pending']
     return render(request, 'list_tasks.html', {
@@ -52,30 +60,53 @@ def list_tasks(request):
 def edit_task(request, task_id):
     """Opens a task for viewing or editing."""
 
-    task = w.get_task(id=task_id)
+    id, task = w.get_task(id=task_id)
 
     if request.method == "POST":
-        form = TaskForm(request.POST, label_suffix='')
+        form = EditTaskForm(request.POST, label_suffix='')
         if form.is_valid():
-            # Add the necessary UDAs into attributes later
-            attributes = ['description', 'project', 'priority', 'due', 'recur',
-                          'until', 'wait', 'scheduled', 'depends',
-                          'annotations', 'tags']
-            for attribute in attributes:
-                a = form.cleaned_data[attribute]
-                # if a and (task[1][attribute] != a):
-                task[1][attribute] = a
-            # The task_update function only accepts a task object's dictionary,
-            # which is the second object in the task's tuple.
-            w.task_update(task[1])
+            # Write non-tag attributes to the task
+            non_tag_fields = ['description',
+                              'priority',
+                              'project',
+                              'due',
+                              'recur',
+                              'until',
+                              'wait']
+            for attribute in non_tag_fields:
+                value = form.cleaned_data[attribute]
+                task[attribute] = value
+            # Create the tag attribute in the format which taskw expects and
+            # write it to the task
+            tag_fields = ['tag_view',
+                          'tag_order',
+                          'tag_time']
+            tags = []
+            for attribute in tag_fields:
+                value = form.cleaned_data[attribute]
+                tags.append(value)
+            task['tags'] = tags
+            # Update the task
+            w.task_update(task)
             return HttpResponseRedirect(reverse('list-tasks'))
     else:
-        form = TaskForm(task[1])
+        # Add each individual tag item to the task object so that they are
+        # displayed in the form
+        tags = task['tags']
+        task['tag_view'] = tags[0]
+        task['tag_order'] = tags[1]
+        task['tag_time'] = tags[2]
+        # Instantiate the form with the task dictionary containing the seperate
+        # tags
+        form = EditTaskForm(task)
 
     return render(request, 'edit_task.html', {
                            'task_id': task_id,
                            'form': form,
                            })
+
+# Add a view which annotates and denotates tasks, as this can't be done by the
+# task_update() method
 
 
 def delete_task(request, task_id):
