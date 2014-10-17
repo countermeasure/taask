5f2d3ed783448ce962b192bfe3657fd1f71c59c3
Curtis <curtis@tinbrain.net>
2014-10-18 00:19:06 +1100

    Tidy with flake8

--- tasks/templatetags/customtags.py
+++ tasks/templatetags/customtags.py
@@ -10,7 +10,7 @@ register = template.Library()
 def inputclass(field, class_to_add):
     """Allows a class or classes to be added to a form input field."""
 
-    return field.as_widget(attrs={"class":class_to_add})
+    return field.as_widget(attrs={"class": class_to_add})
 
 
 @register.filter(name='labelclass')
@@ -18,20 +18,23 @@ def labelclass(field, class_to_add):
     """Allows a class or classes to be added to a form tag."""
 
     field_string = unicode(field)
-    existing_class_regex = re.compile(r'(?<=class=["\'])(.*?)(?=["\'])')
+    existing_class_regex = re.compile(r'''(?<=class=["'])(.*?)(?=["'])''')
     match = existing_class_regex.search(field_string)
     if match:
-        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (class_to_add, class_to_add,
-                                                    class_to_add, class_to_add),
-                                                    match.group(1))
-        print match.group(1)
+        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (
+            class_to_add,
+            class_to_add,
+            class_to_add,
+            class_to_add
+            ), match.group(1))
         if not m:
-            return mark_safe(existing_class_regex.sub(match.group(1) + " " +
-                             class_to_add, field_string))
+            field_string = existing_class_regex.sub(
+                match.group(1) + " " + class_to_add,
+                field_string
+            )
     else:
-        return mark_safe(field_string.replace('>',
-            ' class="%s">' % class_to_add, 1))
-    return value
+        field_string = field_string.replace('>', ' class="%s">' % class_to_add, 1)
+    return mark_safe(field_string)
 
 
 @register.filter(name='autofocus')
