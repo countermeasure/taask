24fc619c830b3eb59a7b090e7713077b42b350a5
Sky <sky@skychristensen.com>
2014-10-19 13:56:34 +1100

    Added STATIC_ROOT to settings

--- taask/settings.py
+++ taask/settings.py
@@ -93,6 +93,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
 
+STATIC_ROOT = os.environ['TAASK_STATIC_ROOT']
+
 # Dates
 # https://docs.djangoproject.com/en/1.7/ref/settings/#date-format
 
