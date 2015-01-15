dee579d98b55f88ba9f43f47f15e7506a0016661
Sky <sky@skychristensen.com>
2015-01-15 22:15:52 +1100

    Scheduled tasks move to Inbox on their scheduled date

--- tasks/utils.py
+++ tasks/utils.py
@@ -1,3 +1,8 @@
+from datetime import date
+
+from django.core.signals import request_finished
+from django.dispatch import receiver
+
 from models import Task
 
 
@@ -20,3 +25,18 @@ def get_task_count():
         task_count[view] = count
 
     return task_count
+
+
+@receiver(request_finished)
+def manage_scheduled_tasks(sender, **kwargs):
+    """Moves scheduled tasks which are scheduled for today or
+    earlier into the 'Today' view.
+    """
+    scheduled_tasks = Task.objects.filter(view='scheduled')
+    today = date.today()
+
+    for task in scheduled_tasks:
+        if task.scheduled <= today:
+            task.scheduled = None
+            task.view = 'today'
+            task.save()
