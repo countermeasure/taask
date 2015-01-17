5caea79e7b673de492832c1756461084fd6220da
Sky <sky@skychristensen.com>
2015-01-17 15:10:21 +1100

    Added management command to create text file and sqlite3 snapshots

---
+++ tasks/management/commands/snapshot.py
@@ -0,0 +1,79 @@
+import errno
+import os
+import shutil
+
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from tasks.models import Task
+
+
+class Command(BaseCommand):
+    help = 'Exports all tasks to a text file'
+
+    def handle(self, *args, **options):
+        HOME = os.environ['HOME']
+        current_datetime = datetime.now()
+        tasks = Task.objects.all().order_by('id').reverse()
+
+        # Generate the snapshot file contents
+        file_contents = '### TAASK TASKS ###\n'
+        file_contents += '# Generated on %s\n\n' % \
+            current_datetime.strftime('%d-%b-%Y @ %H:%M:%S')
+        for task in tasks:
+            contexts = []
+            for context in task.context.all():
+                contexts.append(str(context))
+            task_string = '[description:"%s"\n' % task.description
+            task_string += ' id:"%s"\n' % task.id
+            if task.underway:
+                task_string += ' underway:"%s"\n' % task.underway
+            task_string += ' view:"%s"\n' % task.view
+            if task.time:
+                task_string += ' time:"%s"\n' % task.time
+            if task.project:
+                task_string += ' project:"%s"\n' % task.project
+            if contexts:
+                task_string += ' context:"%s"\n' % contexts
+            if task.priority:
+                task_string += ' priority:"%s"\n' % task.priority
+            if task.scheduled:
+                task_string += ' scheduled:"%s"\n' % task.scheduled
+            if task.deadline:
+                task_string += ' deadline:"%s"\n' % task.deadline
+            if task.frequency:
+                task_string += ' frequency:"%s"\n' % task.frequency
+            if task.starts:
+                task_string += ' starts:"%s"\n' % task.starts
+            if task.ends:
+                task_string += ' ends:"%s"\n' % task.ends
+            if task.task:
+                task_string += ' task:"%s"\n' % task.task
+            if task.notes:
+                task_string += ' notes:"%s"\n' % task.notes
+            task_string += ' created:"%s"\n' % task.created
+            task_string += ' modified:"%s"\n' % task.modified
+            if task.completed:
+                task_string += ' completed:"%s"\n' % task.completed
+            task_string += ']\n'
+            file_contents += task_string
+
+        # Make sure the $HOME/.taask directory exists
+        try:
+            os.makedirs('%s/.taask/snapshots' % HOME)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
+        # Create the snapshot file
+        textfile = '%s/.taask/snapshots/taask_%s.txt' % \
+            (HOME, current_datetime.strftime('%d%b%Y_at_%H%M%S'))
+        with open(textfile, 'w') as f:
+            f.write(file_contents)
+
+        # Create the database snapshot
+        database= '%s/.taask/taask.sqlite3' % HOME
+        database_snapshot = '%s/.taask/snapshots/taask_%s.sqlite3' % \
+            (HOME, current_datetime.strftime('%d%b%Y_at_%H%M%S'))
+        shutil.copy(database, database_snapshot)
